c4ebf860114658df48890a54d3f99f38a92d6e44
rakshit1991 <rakshitdayal.allamraju@gmail.com>
2017-02-17 15:30:13 -0600

    lag test edits

--- lidarscan.py
+++ lidarscan.py
@@ -11,13 +11,14 @@ import pyqtgraph.opengl as gl
 import pyqtgraph as pg
 
 from numpy import size,shape,zeros
+from numpy import append, concatenate, delete
 from LidarProcesses import *
 
 #defines
 N_DATA_BLOCKS = 12
 
 def main():
-    global lidarscatter, lid
+    global lidarscatter, lid, frame_prev, frame_prev_len,ucount,flag
     IP = '192.168.1.71' # IP of recving computer
     PORT_DATA = 2368
     PORT_POS = 8308
@@ -34,17 +35,19 @@ def main():
     #t_data.start()
     #t_parse.start()
     #t_write.start()
-
-
+    frame_prev = empty(shape=(0,7), dtype=float)
+    frame_prev_len = shape(frame_prev)[0]
+    ucount = 0
+    flag = False
+    
     lid = LIDAR(IP,PORT_DATA)
     
     app=QtGui.QApplication([])
     w = gl.GLViewWidget()
     w.opts['distance'] = 20
     layout = QtGui.QGridLayout()
+    
     w.setLayout(layout)
-    
-    
     w.show()
     w.setWindowTitle('GE-OSU Matrice 600 VLP16 Custom Lidar Scan')
 
@@ -54,7 +57,6 @@ def main():
 
     axes = gl.GLAxisItem(glOptions='opaque')
     w.addItem(axes)
-
     pos1 = zeros(shape=(10000,3))
     lidarscatter = gl.GLScatterPlotItem(pos=pos1, color=(0.2,0.4,0.5,0.4),size=2,pxMode=True)
     
@@ -69,21 +71,34 @@ def main():
 
 
 def update():
-    global lidarscatter, lid
-    #FILE_NAME = 'LIDAR_DATA.txt'
-    #f_handle = file(FILE_NAME, 'a')
-    
+    global lidarscatter, lid, frame_prev, frame_prev_len, ucount,flag
+        
     lid.pull_data()
-    lid.create_frame(10000)
+    lid.create_frame(38400)
 
     if len(lid.fstore) > 0:
         DATA = lid.fstore.pop(0)
-        #savetxt(f_handle, DATA)
+        
+        DATA = concatenate((frame_prev, DATA), axis=0)
+        
+        #if flag == True:
+            #print frame_prev_len
+        #    DATA = delete(DATA,xrange(34800),axis=0)
+        #    flag = False
+        #DATA = get_uniques(DATA)
         pos1 = DATA[:,0:3]
         color1 = DATA[:,3:]
+        
         lidarscatter.setData(pos=pos1, color=color1, size=2.5, pxMode=True)
-        #f_handle.close()
-    
+        
+        frame_prev = DATA
+        #frame_prev_len = shape(DATA)
+        #print frame_prev_len
+        #if ucount > 3:
+        #    flag = True
+        #    ucount = 0
+        #ucount +=1
+            
 
 if __name__ == '__main__':
     main()
